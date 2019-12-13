8ca6d435975ba68022071a8e0c6b151fc2f9e069
Trevor Osterman <trevormosterman@gmail.com>
2019-12-13 09:45:05 -0700

    setting up server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
+const { PORT, DATABASE_URL } = require("./config");
+const knex = require("knex");
+const cors = require("cors");
 const app = require("./app");
-const { PORT } = require("./config");
+app.use(cors());
+const db = knex({
+  client: "pg",
+  connection: DATABASE_URL
+});
+
+app.set("db", db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
