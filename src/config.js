5cd35d4216fb8821f97477a85877307df0f0224d
Trevor Osterman <trevormosterman@gmail.com>
2020-01-03 08:40:05 -0700

    troubleshooting

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgresql://TrevorOsterman@localhost/StackNote"
+    "postgres://spmxycxglesvva:07c1c778d83781a3cd9d343a822e5b87b41c0fc271ef8b1f3c5d3db304a8d77e@ec2-107-22-216-123.compute-1.amazonaws.com:5432/d7ck4uiprbrcg1"
 };
