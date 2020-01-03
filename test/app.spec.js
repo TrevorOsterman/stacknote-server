6cd62494f071b9b08ee7abdec24b466d6db22da3
Trevor Osterman <trevormosterman@gmail.com>
2020-01-03 16:24:26 -0700

    knex instance created in test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,6 +1,16 @@
+const knex = require("knex");
 const app = require("../src/app");
 
 describe("App", () => {
+  let db;
+  before("make knex instance", () => {
+    const db = knex({
+      client: "pg",
+      connection: "postgresql://TrevorOsterman@localhost/StackNote"
+    });
+    app.set("db", db);
+  });
+
   it("GET /api/notes responds with 200 containing an array", () => {
     return supertest(app)
       .get("/api/notes")
