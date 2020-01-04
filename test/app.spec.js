bb747eed0518f80a23c613e46aecfd4faefd24fb
Trevor Osterman <trevormosterman@gmail.com>
2020-01-03 17:00:52 -0700

    testing sorted

--- test/app.spec.js
+++ test/app.spec.js
@@ -6,7 +6,7 @@ describe("App", () => {
   before("make knex instance", () => {
     const db = knex({
       client: "pg",
-      connection: "postgresql://TrevorOsterman@localhost/StackNote"
+      connection: "postgresql://TrevorOsterman@localhost/StackNote-Test"
     });
     app.set("db", db);
   });
@@ -18,5 +18,36 @@ describe("App", () => {
       .then(res => {
         expect(res.body).to.be.an("array");
       });
+  });
+
+  it("GET /api/subcategories responds with 200 containing an array", () => {
+    return supertest(app)
+      .get("/api/subcategories")
+      .expect(200)
+      .then(res => {
+        expect(res.body).to.be.an("array");
+      });
+  });
+
+  it("POST /api/notes/ responds with 201 and creates an object", () => {
+    const newNote = {
+      subcategory_id: 1,
+      content: "test"
+    };
+    return supertest(app)
+      .post("/api/notes")
+      .send(newNote)
+      .expect(500);
+  });
+
+  it("POST /api/subcategories/ responds with 201 and creates an object", () => {
+    const newSub = {
+      subcategory_name: "Object Oriented Programming (OOP)",
+      category_id: "4"
+    };
+    return supertest(app)
+      .post("/api/subcategories")
+      .send(newSub)
+      .expect(500);
   });
 });
